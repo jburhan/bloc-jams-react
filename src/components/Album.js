0bbd5a682af422abbb406d74867420de2e9f7a8c
jburhan <jimmyhburhan@gmail.com>
2017-12-10 00:36:15 -0800

    react-router-audio checkpoint

--- src/components/Album.js
+++ src/components/Album.js
@@ -12,12 +12,41 @@ class Album extends Component {
     });
 
     this.state = {
-      album: album
-
+      album: album,
+      album: album,
+      currentSong: album.songs[0],
+      isPLaying: false
     };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
+  }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPLaying: true });
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPLaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
 
   }
 
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song);}
+      this.play();
+    }
+  }
 
   render () {
     return (
@@ -39,7 +68,7 @@ class Album extends Component {
           <tbody>
 
             { this.state.album.songs.map( (song, index) =>
-              <tr key={index} >
+              <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
                 <td className="song-actions">
                   <button>
                     <span className="song-number">{index+1}</span>
